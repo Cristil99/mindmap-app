527600d0b1964aac2e553b93090e4749f8144c8d
piiouu5689000 <138786849+piiouu5689000@users.noreply.github.com>
2025-11-27 10:18:55 +0800

    Initial Solid + Vite project

---
+++ mindmap-app/src/App.tsx
@@ -0,0 +1,14 @@
+import { createSignal } from 'solid-js'
+import solidLogo from './assets/solid.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = createSignal(0)
+
+  return (
+    <h1>Hello Solid.js!</h1>
+  )
+}
+
+export default App
